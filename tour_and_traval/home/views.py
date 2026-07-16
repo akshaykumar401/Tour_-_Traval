2d058330222060f69179086b67d39bfeaa840390
akshaykumar401 <akshaykumar835103@gmail.com>
2026-07-16 15:36:14 +0530

    dispplay the packages on the home page and Tours page

--- tour_and_traval/home/views.py
+++ tour_and_traval/home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+# pyrefly: ignore [missing-import]
+from packages.views import packages_page_data_inJSON
 
 # Create your views here.
 def home(request):
-    return render(request, "home/home_page.html")
+    packages_data = packages_page_data_inJSON(request)[:3] # [:3] -> Slice the top 3 data
+    return render(request, "home/home_page.html", {'package_data': packages_data})
--- tour_and_traval/packages/views.py
+++ tour_and_traval/packages/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Packages
 
 # Create your views here.
+def packages_page_data_inJSON(request):
+  packages = Packages.objects.prefetch_related('images').all()
+  data = []
+  for p in packages:
+    image_obj = p.images.first()
+    image_url = image_obj.image.url if image_obj and image_obj.image else None
+    data.append({
+      'id': p.id,
+      'tag_line': p.tag_line,
+      'category': p.category,
+      'location': p.location,
+      'mini_title': p.mini_title,
+      'total_days': p.total_days,
+      'total_nights': p.total_nights,
+      'starting_price': p.starting_price,
+      'max_people': p.max_people,
+      'main_title': p.main_title,
+      'description': p.description,
+      'created_at': p.created_at,
+      'image': image_url
+    })
+  return data
+
 def packages_page(request):
-  return render(request, 'packages/packages_page.html')
+  data = packages_page_data_inJSON(request)
+  return render(request, 'packages/packages_page.html', {'package_data': data})
 
 def packages_detail_page(request, packages_id):
   return render(request, 'packages/packages_detail_page.html', {'packages_id': packages_id})
