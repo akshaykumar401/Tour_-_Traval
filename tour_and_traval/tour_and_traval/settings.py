8f5ac3fff8b7dd19acce5425427c7a4ef59b35ed
akshaykumar401 <akshaykumar835103@gmail.com>
2026-07-19 21:58:04 +0530

    changing SQLite to Postgress DB

--- tour_and_traval/tour_and_traval/settings.py
+++ tour_and_traval/tour_and_traval/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,11 +108,20 @@ WSGI_APPLICATION = "tour_and_traval.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+# Fail immediately with a clear explanation if the variable is missing
+if not DATABASE_URL:
+    raise ImproperlyConfigured(
+        "DATABASE_URL environment variable is missing! "
+        "Please set it in your terminal before running commands."
+    )
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
